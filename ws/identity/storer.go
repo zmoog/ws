a863ddcbf9ea7c1f31d8f4e43f5cb8f776d5c17a
Maurizio Branca <maurizio.branca@gmail.com>
2025-09-27 19:16:28 +0200

    Add simple in-memory storer for testing (#6)
    * Add simple in-memory storer for testing
    
    * Check for missing token and add factory method

--- ws/identity/storer.go
+++ ws/identity/storer.go
@@ -70,3 +70,30 @@ func (s *tokenStorer) getSettingsPath() (string, error) {
 	}
 	return filepath.Join(homeDir, ".ws", "identity"), nil
 }
+
+// inMemoryStorer is a concrete implementation of Storer
+// that stores the token in memory.
+type inMemoryStorer struct {
+	token *Token
+}
+
+// NewInMemoryStorer creates a new in-memory storer.
+func NewInMemoryStorer() Storer {
+	return &inMemoryStorer{
+		token: nil,
+	}
+}
+
+// StoreToken stores a token in the memory.
+func (s *inMemoryStorer) StoreToken(token Token) error {
+	s.token = &token
+	return nil
+}
+
+// GetToken retrieves a token from the memory.
+func (s *inMemoryStorer) GetToken() (Token, bool, error) {
+	if s.token == nil {
+		return Token{}, false, nil
+	}
+	return *s.token, true, nil
+}
